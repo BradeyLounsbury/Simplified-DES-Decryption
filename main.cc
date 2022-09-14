9e7a83b088a262290ee3ea66399b74fda68c3c23
Bradey Lounsbury <bl396918@ohio.edu>
2022-09-14 01:04:18 -0400

    using bitset now

--- main.cc
+++ main.cc
@@ -5,97 +5,113 @@
 
 using namespace std;
 
-int* p10(int key_bits[10]);
-int* shift_1(int half_key[5]);
-int* shift_2(int half_key[5]);
-int* p8(int* left, int* right);
+void p10(bitset<10> &key_bits);
+bitset<5> shift(bitset<5> half_key);
+bitset<8> p8(bitset<5> left, bitset<5> right);
 
 
 int main() {
     string cipher_text, key;
-    int cipher_text_bits[8], key_bits[10];
+    // int cipher_text_bits[8], key_bits[10];
 
-    getline(cin, cipher_text);
+    // getline(cin, cipher_text);
     getline(cin, key);
     
-    for (size_t i = 0; i < 10; i++) {
-        if(key[i] == '0') {
-            key_bits[i] = 0;
-        }
-        else if (key[i] == '1') {
-            key_bits[i] = 1;
-        }
-        else {
-            cout << "key incorrectly formatted\n";
-            return -1;
-        }
+    if (!(key.length() == 10)) {
+        cout << "key not 10 bits\n";
+        return -1;
     }
+    
+    bitset<10> key_bits (key);
 
-    int* key_perm = p10(key_bits);
-    int left_half[5], right_half[5];
+    p10(key_bits);
+    bitset<5> left_half, right_half;
     
     for (size_t i = 0; i < 5; i++) {
-        left_half[i] = key_bits[i];
-        right_half[i] = key_bits[i+5];
+        right_half[i] = key_bits[i];
+        left_half[i] = key_bits[i+5];
     }
     
-    int* left_shift_1 = shift_1(left_half);
-    int* right_shift_1 = shift_1(right_half);
-    int* k1 = p8(left_shift_1, right_shift_1);
+    bitset<5> left_shift_1 = shift(left_half);
+    bitset<5> right_shift_1 = shift(right_half);
+    bitset<8> k1 = p8(left_shift_1, right_shift_1);
 
-    int* left_shift_2 = shift_2(left_half);
-    int* right_shift_2 = shift_2(right_half);
-    int* k2 = p8(left_shift_2, right_shift_2);
+    bitset<5> left_shift_2 = shift(left_shift_1);
+    left_shift_2 = shift(left_shift_2);
+    bitset<5> right_shift_2 = shift(right_shift_1);
+    right_shift_2 = shift(right_shift_2);
+    bitset<8> k2 = p8(left_shift_2, right_shift_2);
 
-
+    cout << "key: " << key << endl;
+    cout << "key_bits after p10: " << key_bits << endl;
+    cout << "left half: " << left_half << endl;
+    cout << "right half: " << right_half << endl;
+    cout << "left half shifted 1: " << left_shift_1 << endl;
+    cout << "right half shifted 1: " << right_shift_1 << endl;
+    cout << "left half shifted 2: " << left_shift_2 << endl;
+    cout << "right half shifted 2: " << right_shift_2 << endl;
+    cout << "k1: " << k1 << endl;
+    cout << "k2: " << k2 << endl;
     
     return 0;
 }
 
-int* p10(int key_bits[10]) {
-    static int key_perm[10];
-    key_perm[0] = key_bits[2];
-    key_perm[1] = key_bits[4];
-    key_perm[2] = key_bits[1];
-    key_perm[3] = key_bits[6];
-    key_perm[4] = key_bits[3];
-    key_perm[5] = key_bits[9];
-    key_perm[6] = key_bits[0];
-    key_perm[7] = key_bits[8];
-    key_perm[8] = key_bits[7];
-    key_perm[9] = key_bits[5];
-    return key_perm;
+void p10(bitset<10> &key_bits) {
+    // 9 8 7 6 5 4 3 2 1 0
+    // 5 7 8 0 9 3 6 1 4 2
+    bitset<10> key_perm = key_bits;
+    key_bits[0] = key_perm[4];
+    key_bits[1] = key_perm[2];
+    key_bits[2] = key_perm[1];
+    key_bits[3] = key_perm[9];
+    key_bits[4] = key_perm[0];
+    key_bits[5] = key_perm[6];
+    key_bits[6] = key_perm[3];
+    key_bits[7] = key_perm[8];
+    key_bits[8] = key_perm[5];
+    key_bits[9] = key_perm[7];
 }
 
-int* shift_1(int half_key[5]) {
-    static int shifted[5];
-    shifted[0] = half_key[1];
-    shifted[1] = half_key[2];
-    shifted[2] = half_key[3];
-    shifted[3] = half_key[4];
-    shifted[4] = half_key[0];
-    return shifted;
+bitset<5> shift(bitset<5> half_key) {
+    // static int shifted[5];
+    // shifted[0] = half_key[1];
+    // shifted[1] = half_key[2];
+    // shifted[2] = half_key[3];
+    // shifted[3] = half_key[4];
+    // shifted[4] = half_key[0];
+    if (half_key.test(4)) {
+        half_key = half_key<<1;
+        half_key.set(0,1);
+    }
+    else {
+        half_key = half_key<<1;
+    }
+    return half_key;
 }
 
-int* shift_2(int half_key[5]) {
-    static int shifted[5];
-    shifted[0] = half_key[2];
-    shifted[1] = half_key[3];
-    shifted[2] = half_key[4];
-    shifted[3] = half_key[0];
-    shifted[4] = half_key[1];
-    return shifted;
-}
+bitset<8> p8(bitset<5> left, bitset<5> right) {
+    // static int perm[8];
+    // perm[0] = *(right);
+    // perm[1] = *(left + 2);
+    // perm[2] = *(right + 1);
+    // perm[3] = *(left + 3);
+    // perm[4] = *(right + 2);
+    // perm[5] = *(left + 4);
+    // perm[6] = *(right + 4);
+    // perm[7] = *(right + 3);
+    // return perm;
 
-int* p8(int* left, int* right) {
-    static int perm[8];
-    perm[0] = *(right);
-    perm[1] = *(left + 2);
-    perm[2] = *(right + 1);
-    perm[3] = *(left + 3);
-    perm[4] = *(right + 2);
-    perm[5] = *(left + 4);
-    perm[6] = *(right + 4);
-    perm[7] = *(right + 3);
+    // left = [5, 4, 3, 2, 1]
+    // right = [10, 9, 8, 7, 6]
+    bitset<8> perm;
+
+    perm[7] = right[4];
+    perm[6] = left[2];
+    perm[5] = right[3];
+    perm[4] = left[1];
+    perm[3] = right[2];
+    perm[2] = left[0];
+    perm[1] = right[0];
+    perm[0] = right[1];
     return perm;
 }
